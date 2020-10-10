4c7f7ea59d081665b9aefd1a926e0cf622c73ed6
Oliver <45573843+owehmer@users.noreply.github.com>
2020-10-10 20:38:58 +0200

    Using constants for JWT Config and extend Claim (#6)

--- server/UserApi/UserApi/JwtConfig.cs
+++ server/UserApi/UserApi/JwtConfig.cs
@@ -1,8 +1,42 @@
-﻿namespace UserApi.Config
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace UserApi.Config
 {
     public static class JwtConfig
     {
         public static readonly string KEY = "my_secret_key_12345";
         public static readonly string ISSUER = "http://mysite.com";
+        
+        public static readonly string JWT_USER_NAME_PROP = "name";
+        public static readonly string JWT_USER_ID_PROP = "userId";
+        public static readonly string JWT_USER_ACTIVE_PROP = "active";
+        
+        public static int? GetUserId(this ClaimsIdentity identity)
+        {
+            var userIdValue = identity.Claims?.FirstOrDefault(c => c.Type == JWT_USER_ID_PROP)?.Value;
+            if (int.TryParse(userIdValue, out var userId))
+            {
+                return userId;
+            }
+
+            return null;
+        }
+        
+        public static string GetUserName(this ClaimsIdentity identity)
+        {
+            return identity.Claims?.FirstOrDefault(c => c.Type == JWT_USER_NAME_PROP)?.Value;
+        }
+        
+        public static bool GetUserActive(this ClaimsIdentity identity)
+        {
+            var activeValue = identity.Claims?.FirstOrDefault(c => c.Type == JWT_USER_ACTIVE_PROP)?.Value;
+            if (bool.TryParse(activeValue, out var active))
+            {
+                return active;
+            }
+
+            return false;
+        }
     }
 }
